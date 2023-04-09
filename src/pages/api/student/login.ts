db0448321361903cd85636e33c3799e91a35e3ee
aritroCoder <ogcodinggod@gmail.com>
2023-04-09 20:18:49 +0530

    added student login

---
+++ src/pages/api/student/login.ts
@@ -0,0 +1,59 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import { connectToSql, disconnect } from '../../../utils/db'
+
+const SECRET_KEY = process.env.SECRET_KEY || 'secret' ;
+/**
+  * Request body:
+  * {
+  *  "email": "johncena@gawd.com",
+  *  "password": "123456789"
+  *  }
+  * */
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Object>
+) {
+  try {
+    const connection = connectToSql();
+    // check if record exists
+    connection.query('SELECT * FROM students WHERE email = ?', [req.body.email], async function (error: Object, results: any, _fields: any) {
+      if (error) {
+        res.json({'error': error})
+      }
+
+      if(results.length == 0){
+        res.json({'error': 'Student does not exist'})
+      } else {
+        const match = await bcrypt.compare(req.body.password, results[0].password);
+        if(!match){
+          res.json({'error': 'Incorrect credentials'})
+        } else {
+          let col = results[0]
+          var data = {
+            rollno: col.rollno,
+            first_name: col.first_name,
+            last_name: col.last_name,
+            email: col.email,
+            gpa: col.gpa,
+            department: col.department,
+            roles: col.roles,
+            gender: col.gender,
+            cv: col.cv,
+            image: col.image
+          }
+          const token = jwt.sign({
+            username: data.first_name,
+            email: data.email
+          }, SECRET_KEY, {});
+          res.json({'token': token, 'data': data})
+        }
+      }
+    disconnect(connection);
+    });
+  } catch (error) {
+    res.json({'error': error})
+  }
+}
