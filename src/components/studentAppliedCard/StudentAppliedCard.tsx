a91bd9279c888be43b152bbd22e3b7e953021d23
aritroCoder <ogcodinggod@gmail.com>
2023-04-16 00:53:36 +0530

    added calling function s

--- src/components/studentAppliedCard/StudentAppliedCard.tsx
+++ src/components/studentAppliedCard/StudentAppliedCard.tsx
@@ -1,11 +1,20 @@
-import React, {useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./StudentAppliedCard.module.css";
-const StudentAppliedCard = ({studentRoll,name,email,department,cpi,skills,role}) => {
-  const [companyId, setCompanyId] = useState('')
+const StudentAppliedCard = ({
+  studentRoll,
+  name,
+  email,
+  department,
+  cpi,
+  skills,
+  role,
+  callfunction
+}) => {
+  const [companyId, setCompanyId] = useState("");
   useEffect(() => {
     const compDetails = JSON.parse(localStorage.getItem("profile"));
-    setCompanyId(compDetails.id)
-  }, [])
+    setCompanyId(compDetails.id);
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -13,7 +22,7 @@ const StudentAppliedCard = ({studentRoll,name,email,department,cpi,skills,role})
       method: "POST",
       body: JSON.stringify({
         company_id: companyId,
-        role_offered:role,
+        role_offered: role,
         student_id: studentRoll,
       }),
       headers: {
@@ -22,22 +31,31 @@ const StudentAppliedCard = ({studentRoll,name,email,department,cpi,skills,role})
     });
     const data = await res.json();
     if (data.error) {
-      console.log(data.error)
+      console.log(data.error);
     } else {
       console.log("Success");
     }
+    callfunction()
   };
-  
+
   return (
     <div className={styles.card}>
       <div className={styles.card_border_top}></div>
       <div className={styles.img}></div>
       <span> {name}</span>
       <p className={styles.job}> {email}</p>
-      <p className={styles.job}><strong>DEPARTMENT:</strong> {department}</p>
-      <p className={styles.job}><strong>CPI:</strong> {cpi}</p>
-      <p className={styles.job}><strong>SKILLS:</strong> {skills}</p>
-      <p className={styles.job}><strong>ROLE APPLIED:</strong> {role}</p>
+      <p className={styles.job}>
+        <strong>DEPARTMENT:</strong> {department}
+      </p>
+      <p className={styles.job}>
+        <strong>CPI:</strong> {cpi}
+      </p>
+      <p className={styles.job}>
+        <strong>SKILLS:</strong> {skills}
+      </p>
+      <p className={styles.job}>
+        <strong>ROLE APPLIED:</strong> {role}
+      </p>
       <button onClick={handleSubmit}> Recruit</button>
     </div>
   );
--- src/pages/recruiter/main.tsx
+++ src/pages/recruiter/main.tsx
@@ -1,83 +1,89 @@
-import React,{useEffect,useState} from 'react'
-import StudentAppliedCard from '@/components/studentAppliedCard/StudentAppliedCard'
-import Navbar from '@/components/recruiterNavbar'
-import { useRouter } from 'next/router'
+import React, { useEffect, useState } from "react";
+import StudentAppliedCard from "@/components/studentAppliedCard/StudentAppliedCard";
+import Navbar from "@/components/recruiterNavbar";
+import { useRouter } from "next/router";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 const main = () => {
-  const [companyId, setCompanyId] = useState('')
-  const [data, setData] = useState()
-  const router = useRouter()
+  const [companyId, setCompanyId] = useState("");
+  const [data, setData] = useState();
+  const router = useRouter();
+
+  const fetchStudent = async () => {
+    const res = await fetch("/api/company/applications", {
+      method: "POST",
+      body: JSON.stringify({
+        company_id: companyId,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+    if (data.error) {
+      // console.log(data.error);
+      toast.error(data.error, {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    } else {
+      // router.push("/stud/main");
+      // console.log(data.data);
+      console.log(data);
+      setData(data);
+    }
+  };
+
   useEffect(() => {
     if (!localStorage.getItem("token")) {
       router.push("/company/signin");
-    } 
+    }
     // setCompanyId(JSON.parse(localStorage.getItem("profile")).company_id)
-    
     const compDetails = JSON.parse(localStorage.getItem("profile"));
-    setCompanyId(compDetails.id)
+    setCompanyId(compDetails.id);
+  }, []);
 
-    // console.log('compDetails', compDetails)
-  
-  }, [])
   useEffect(() => {
-    const fetchStudent = async () => {
-      const res = await fetch("/api/company/applications", {
-        method: "POST",
-        body: JSON.stringify({
-          company_id: companyId,
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      const data = await res.json();
-      if (data.error) {
-        // console.log(data.error);
-        toast.error(data.error, {
-          position: "top-right",
-          autoClose: 3000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "light",
-        });
-      } else {
-        // router.push("/stud/main");
-        // console.log(data.data);
-        console.log(data)
-        setData(data);
-    }
-  }
-  const fetchid = () =>{
-    const compDetails = JSON.parse(localStorage.getItem("profile"));
-    setCompanyId(compDetails.id)
-    console.log(compDetails)
-    }
-  fetchStudent()
-  fetchid()
+    fetchStudent();
+  }, [companyId]);
 
-  }, [companyId])
-  
   return (
     <>
-    <Navbar/>
-    <div style={{paddingTop: '50px'}}>
-      <h2 className="text-5xl font-semibold text-center my-7">Hello, Welcome to the TPC IITP Company's Portal</h2>
-      <h3 className="text-4xl font-semibold text-center my-7">Here is the list of studnets who have applied to your company</h3>
-      <div className="flex flex-col lg:flex-row justify-center items-center my-10 gap-20 flex-wrap">
-        {data?console.log(data):""}
-        {data && data.map((item) => {
-          return <StudentAppliedCard studentRoll={item.rollno} name={item.first_name+" "+item.last_name} cpi={item.gpa} department={item.department} email={item.email} skills={item.roles} role={item.role_applied}/>
-        })}
-
-
+      <Navbar />
+      <div style={{ paddingTop: "50px" }}>
+        <h2 className="text-5xl font-semibold text-center my-7">
+          Hello, Welcome to the TPC IITP Company's Portal
+        </h2>
+        <h3 className="text-4xl font-semibold text-center my-7">
+          Here is the list of studnets who have applied to your company
+        </h3>
+        <div className="flex flex-col lg:flex-row justify-center items-center my-10 gap-20 flex-wrap">
+          {data ? console.log(data) : ""}
+          {data &&
+            data.map((item) => {
+              return (
+                <StudentAppliedCard
+                  studentRoll={item.rollno}
+                  name={item.first_name + " " + item.last_name}
+                  cpi={item.gpa}
+                  department={item.department}
+                  email={item.email}
+                  skills={item.roles}
+                  role={item.role_applied}
+                  callfunction = {fetchStudent()}
+                />
+              );
+            })}
+        </div>
       </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default main
+export default main;
